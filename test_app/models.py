a666f3fd9bfbb8b67e76fecc39d1b56e7326fc77
Anatolii Pozniak <a13101990a@gmail.com>
2023-06-30 20:34:16 +0300

    add Position test model

--- test_app/models.py
+++ test_app/models.py
@@ -1,2 +1,11 @@
 from django.db import models
 
+
+class Position(models.Model):
+    name = models.CharField(max_length=63)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
